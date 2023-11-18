28c701160d90b43775372cfca3e9048815c153d9
徐超越 <993921@qq.com>
2023-11-18 18:24:55 +0800

    support sim800c with usb-ttl serial communicate

--- go.mod
+++ go.mod
@@ -1,3 +1,45 @@
 module AirAccountSmsAdapter
 
 go 1.20
+
+require (
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
+	github.com/totoval/framework v0.11.7
+	k8s.io/apimachinery v0.28.4
+)
+
+require (
+	github.com/certifi/gocertifi v0.0.0-20180905225744-ee1a9a0726d2 // indirect
+	github.com/fsnotify/fsnotify v1.4.7 // indirect
+	github.com/getsentry/raven-go v0.2.0 // indirect
+	github.com/gin-contrib/sentry v0.0.0-20190528141903-48a399dfd39f // indirect
+	github.com/gin-contrib/sse v0.0.0-20190301062529-5545eab6dad3 // indirect
+	github.com/gin-gonic/gin v1.4.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
+	github.com/logrusorgru/aurora v0.0.0-20181002194514-a7b3b318ed4e // indirect
+	github.com/magiconair/properties v1.8.0 // indirect
+	github.com/mattn/go-isatty v0.0.8 // indirect
+	github.com/mitchellh/mapstructure v1.1.2 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml v1.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.4.2 // indirect
+	github.com/spf13/afero v1.1.2 // indirect
+	github.com/spf13/cast v1.3.0 // indirect
+	github.com/spf13/jwalterweatherman v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.4.0 // indirect
+	github.com/ugorji/go v1.1.4 // indirect
+	github.com/ztrue/tracerr v0.3.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
+)
---
+++ infra/sim800c.go
@@ -0,0 +1,183 @@
+package infra
+
+import (
+	"bytes"
+	"github.com/tarm/serial"
+	"github.com/totoval/framework/helpers/log"
+	"github.com/totoval/framework/helpers/toto"
+	"github.com/totoval/framework/helpers/zone"
+	"io"
+	"time"
+)
+
+type Sim800c struct {
+	writer    *serial.Port
+	conf      *serial.Config
+	chB       chan []byte
+	chErr     chan error
+	isReading bool
+}
+
+func NewSim800c(comPort string, baudRate int, readTimeout zone.Duration) (*Sim800c, error) {
+	s := &Sim800c{
+		chB:   make(chan []byte, 50*3), // "+CPMS: \"SM_P\",50,50,\"SM_P\",50,50,\"SM_P\",50,50"
+		chErr: make(chan error, 50*3),
+	}
+	s.conf = &serial.Config{Name: comPort, Baud: baudRate, ReadTimeout: readTimeout}
+
+	var err error
+	if s.writer, err = serial.OpenPort(s.conf); err != nil {
+		return nil, err
+	}
+
+	if err := s.init(); err != nil {
+		return nil, err
+	}
+
+	return s, nil
+}
+func (s *Sim800c) init() error {
+	if err := s.Write([]byte("AT+CMGF=1\r\n")); err != nil {
+		return err
+	}
+	if err := s.Write([]byte("AT+CSCS=\"GSM\"\r\n")); err != nil {
+		return err
+	}
+	if err := s.Write([]byte("AT+CNMI=2,1\r\n")); err != nil {
+		return err
+	}
+	return nil
+}
+func (s *Sim800c) Close() error {
+	defer close(s.chB)
+	defer close(s.chErr)
+	return s.writer.Close()
+}
+func (s *Sim800c) flush() error {
+	return s.writer.Flush()
+}
+func (s *Sim800c) read(b []byte) (int, error) {
+	return s.writer.Read(b)
+}
+func (s *Sim800c) write(b []byte) (int, error) {
+	return s.writer.Write(b)
+}
+func (s *Sim800c) Read2() {
+	if s.isReading {
+		return
+	}
+
+	s.isReading = true // only read once
+	defer func() {
+		s.isReading = false
+	}()
+
+	var b []byte
+	for {
+		_b := make([]byte, 128)
+
+		_n, err := s.read(_b)
+		if err != nil {
+			if err == io.EOF {
+				if len(b) <= 0 {
+					// no message, continue receiving
+					continue
+				}
+
+				if _n <= 0 {
+					// received finished
+					return
+				}
+
+				// len(b) > 0 && _n > 0 received aborted
+				s.chErr <- io.EOF
+				s.chB <- b
+				return
+			}
+			// received error
+			s.chErr <- err
+			return
+		}
+
+		if _n > 0 {
+			b = append(b, _b[:_n]...)
+		}
+
+		time.Sleep(time.Second)
+		if bytes.HasPrefix(b, []byte("\r\n")) && bytes.HasSuffix(b, []byte("\r\n")) {
+			if bytes.Contains(b, []byte("\r\n\r\n")) {
+				__b := bytes.Trim(b, "\r\n")
+				msgArr := bytes.Split(__b, []byte("\r\n\r\n")) // [][data]
+				log.Warn(len(msgArr))
+				for _, msg := range msgArr {
+					log.Warn(msg)
+					s.chB <- msg
+				}
+			} else {
+				// single msg bytes
+				__b := bytes.Trim(b, "\r\n") // data
+				s.chB <- __b
+			}
+
+			break // msg end
+		} else if bytes.HasPrefix(b, []byte("AT+CMGR=")) && bytes.Contains(b, []byte("\r\n")) {
+			bytes.Split(b, []byte("\r\n"))
+			msgArr := bytes.Split(b, []byte("\r\n")) // [][data]
+			for idx, msg := range msgArr {
+				if bytes.HasPrefix(msg, []byte("+CMGR:")) {
+					msg = []byte(string(msg) + "<br />" + string(msgArr[idx+1]))
+					s.chB <- msg
+				}
+			}
+			break
+		}
+	}
+	return
+}
+
+func (s *Sim800c) Read() (n int, b []byte, err error) {
+	for {
+		_b := make([]byte, 128)
+
+		_n, err := s.read(_b)
+		n += _n
+		if _n > 0 {
+			b = append(b, _b[:_n]...)
+		}
+
+		if bytes.HasSuffix(b, []byte("\r\n")) {
+			break
+		}
+
+		if err != nil {
+			if err == io.EOF {
+				if n > 0 {
+					break
+				}
+				continue
+			}
+			return n, nil, err
+		}
+	}
+	return n, b[:n], nil
+}
+
+func (s *Sim800c) Write(b []byte) error {
+	if err := s.flush(); err != nil {
+		return err
+	}
+	n, err := s.write(b)
+	if err != nil {
+		return err
+	}
+	log.Info("Send Bytes", toto.V{"bytes": string(b[:]), "length": n})
+	time.Sleep(time.Millisecond * 200)
+	return nil
+}
+
+func (s *Sim800c) Error() <-chan error {
+	return s.chErr
+}
+func (s *Sim800c) Bytes() <-chan []byte {
+	return s.chB
+}
