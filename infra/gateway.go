bee6f458e3c01eb2c89409ad2053b538e5e220e2
徐超越 <993921@qq.com>
2023-12-24 00:03:13 +0800

    chore: debug log

--- infra/gateway.go
+++ infra/gateway.go
@@ -26,21 +26,23 @@ func (gw *Gateway) PollUnreadMessages() {
 		}
 	}()
 
-	for {
-		time.Sleep(time.Second)
-		select {
-		case b := <-gw.chip.Bytes():
-			log.Info("Incoming data", toto.V{"data": string(b[:])})
-			err := log.Error(gw.parse(b))
-			if err != nil {
-				go log.Debug(fmt.Sprintf("error: %s | %s", err.Error(), string(b[:])))
-			}
-		case err := <-gw.chip.Error():
-			log.Panic(err)
-		default:
+	go func() {
+		for {
 			time.Sleep(time.Second)
+			select {
+			case b := <-gw.chip.Bytes():
+				log.Info("Incoming data", toto.V{"data": string(b[:])})
+				err := log.Error(gw.parse(b))
+				if err != nil {
+					go log.Debug(fmt.Sprintf("error: %s | %s", err.Error(), string(b[:])))
+				}
+			case err := <-gw.chip.Error():
+				log.Panic(err)
+			default:
+				time.Sleep(time.Second)
+			}
 		}
-	}
+	}()
 
 	for {
 		gw.chip.Write([]byte("AT+CMGL=\"REC UNREAD\"\r\n"))
